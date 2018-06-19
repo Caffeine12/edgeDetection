4fdc4a4ce4b3ac6d72affa6662e73fffe5f98b79
Jubair Obhi <jubairku12@gmail.com>
2018-06-19 22:40:18 +0600

    Sobel Operator mask has been applied and output has been shown

--- edgeDetection.m
+++ edgeDetection.m
@@ -13,4 +13,15 @@ dbl = double((R+G+B)/3);
 Final = uint8(dbl);
 Gray = double(Final);
 
-figure,imshow(Final)
+%Applying Sobel gradient algorithm
+
+for i = 1:size(Gray,1)-2
+    for j = 1:size(Gray,2)-2
+        Gx =((2*Gray(i+2,j+1)+Gray(i+2,j)+Gray(i+2,j+2))-(2*Gray(i,j+1)+Gray(i,j)+Gray(i,j+2)));
+        Gy =((2*Gray(i+1,j+2)+Gray(i,j+2)+Gray(i+2,j+2))-(2*Gray(i+1,j)+Gray(i,j)+Gray(i+2,j)));
+        Edg(i,j) = sqrt(Gx.^2+Gy.^2);
+    end
+end
+sobgradient = uint8(Edg);
+figure, imshow(sobgradient);
+title('Sobel Gradient');
